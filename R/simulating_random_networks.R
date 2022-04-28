a7d536ba1cb3d791abde255add9471d42a48cc8b
Dan Riggins <daniel.riggins@protonmail.com>
2022-04-27 20:19:41 -0500

    Scrapped code for simulating random networks

---
+++ R/simulating_random_networks.R
@@ -0,0 +1,22 @@
+# erdos_renyi_sims <-
+#     tibble( 
+#         graph_obj = 
+#             map(
+#                 1:1000,
+#                 ~filter_to_max_connected_component(
+#                     play_erdos_renyi(
+#                         n = gorder(MUC_huttlin_graph),
+#                         m = gsize(MUC_huttlin_graph),
+#                         directed = FALSE 
+#                     )
+#                 )
+#             ) 
+#     )
+# 
+# erdos_renyi_sims <-
+#     erdos_renyi_sims |> 
+#     mutate(
+#         SWI = map(graph_obj, ~qgraph::smallworldIndex(.x))
+#     ) |> 
+#     unnest(SWI) |> 
+#     unnest(SWI)
